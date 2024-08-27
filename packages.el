80c633c08dcd01f27a2e0d0f935eeddf60e2bcc7
iurii plugatarov <plugatariov@gmail.com>
2024-08-28 00:08:46 +0300

    upd

---
+++ packages.el
@@ -0,0 +1,4 @@
+;; Doom's packages are pinned to a specific commit and updated from release to
+;; release. The `unpin!' macro allows you to unpin single packages...
+(package! odin-mode
+  :recipe (:host github :repo "mattt-b/odin-mode"))
